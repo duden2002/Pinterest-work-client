798d72b95e767d8501b911956dcaf6f383e97d9b
Brodyaga <dudenkokirill68@gmail.com>
2025-01-27 14:58:51 +0500

    Fixed for

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext', // Убедитесь, что Vite компилирует файлы в современный JavaScript
+    outDir: 'dist',  // Убедитесь, что указана правильная папка для сборки
+    assetsDir: 'assets',  // Убедитесь, что ассеты правильно направляются
+  },
+  server: {
+    fs: {
+      strict: true,  // Эта настройка может помочь в определенных случаях
+    },
   },
 })
